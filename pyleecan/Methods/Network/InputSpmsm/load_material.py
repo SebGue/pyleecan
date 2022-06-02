b07befeeb65d001f6177934b8bab774be1672c9f
Aicha Maaoui <aicha.maaoui@eomys.com>
2022-06-02 11:06:08 +0200

    [NF] Add a new method load_parameters to load the machine inputs from a .json file
    Signed-off-by: Aicha Maaoui <aicha.maaoui@eomys.com>

--- pyleecan/Methods/Network/InputSpmsm/load_material.py
+++ pyleecan/Methods/Network/InputSpmsm/load_material.py
@@ -4,7 +4,7 @@ from pyleecan.Functions.load import load_json
 
 
 def LoadMaterial(file_path_json):
-    """Load the stator and rotor material characteristics of an SPMSM machine
+    """Load the stator and rotor material characteristics of a PM machine
 
     Parameters
     ----------
@@ -13,80 +13,76 @@ def LoadMaterial(file_path_json):
 
     Returns
     -------
-    magnet_mat: float
+    magnet_mat: dict
         The magnet_remanence and magnet_permeability of the magnet
-    rotor_mat: float
+    rotor_mat: dict
         The linear or non-linear material properties of the rotor
-    stator_mat: float
+    stator_mat: dict
         The linear or non-linear material properties of the stator
-    winding_mat: float
+    winding_mat: dict
         The material properties of the winding and insulation
-    shaft_mat: float
+    shaft_mat: dict
         The linear or non-linear material properties of the shaft
     """
 
-    # Load the SPMSM machine from the .json file
-    SPMSM = load_json.load_json(file_path_json)
+    # Load the PM machine from the .json file
+    PM = load_json.load_json(file_path_json)
 
     # Get the dictionary values with the different motor parameters
-    dict_SPMSM = SPMSM[1]
+    dict_PM = PM[1]
 
     # Get Magnet material properties
     magnet_mat = {
-        "magnet_remanence": dict_SPMSM["rotor"]["magnet"]["mat_type"]["mag"]["Brm20"],
-        "magnet_permeability": dict_SPMSM["rotor"]["magnet"]["mat_type"]["mag"][
-            "mur_lin"
-        ],
+        "magnet_remanence": dict_PM["rotor"]["magnet"]["mat_type"]["mag"]["Brm20"],
+        "magnet_permeability": dict_PM["rotor"]["magnet"]["mat_type"]["mag"]["mur_lin"],
     }
 
     # Get Rotor material properties
-    if dict_SPMSM["rotor"]["mat_type"]["is_isotropic"] == False:
+    if dict_PM["rotor"]["mat_type"]["is_isotropic"] == False:
         rotor_mat = {
-            "rotor_permeance": dict_SPMSM["rotor"]["mat_type"]["mag"]["BH_curve"][
-                "value"
-            ]
+            "rotor_permeance": dict_PM["rotor"]["mat_type"]["mag"]["BH_curve"]["value"]
         }  # non-linear case
     else:
         rotor_mat = {
-            "rotor_permeance": dict_SPMSM["rotor"]["mat_type"]["mag"]["mur_lin"]
+            "rotor_permeance": dict_PM["rotor"]["mat_type"]["mag"]["mur_lin"]
         }  # linear case
 
     # Get Stator material properties
-    if dict_SPMSM["stator"]["mat_type"]["is_isotropic"] == False:
+    if dict_PM["stator"]["mat_type"]["is_isotropic"] == False:
         stator_mat = {
-            "stator_permeance": dict_SPMSM["stator"]["mat_type"]["mag"]["BH_curve"][
+            "stator_permeance": dict_PM["stator"]["mat_type"]["mag"]["BH_curve"][
                 "value"
             ]
         }
     else:
         stator_mat = {
-            "stator_permeance": dict_SPMSM["stator"]["mat_type"]["mag"]["mur_lin"]
+            "stator_permeance": dict_PM["stator"]["mat_type"]["mag"]["mur_lin"]
         }
 
     # Conductor and insulator materials
-    if dict_SPMSM["stator"]["winding"]["conductor"]["cond_mat"]["mag"] != None:
+    if dict_PM["stator"]["winding"]["conductor"]["cond_mat"]["mag"] != None:
         winding_mat = {
-            "winding_material": dict_SPMSM["stator"]["winding"]["conductor"][
-                "cond_mat"
-            ]["mag"]["mur_lin"],
-            "insulation_material": dict_SPMSM["stator"]["winding"]["conductor"][
-                "ins_mat"
-            ]["mag"]["mur_lin"],
+            "winding_material": dict_PM["stator"]["winding"]["conductor"]["cond_mat"][
+                "mag"
+            ]["mur_lin"],
+            "insulation_material": dict_PM["stator"]["winding"]["conductor"]["ins_mat"][
+                "mag"
+            ]["mur_lin"],
         }
     else:
         winding_mat = {"winding_material": "Not defined"}
 
     # Get Shaft material properties
-    if dict_SPMSM["shaft"] != None:
-        if dict_SPMSM["shaft"]["mat_type"]["is_isotropic"] == False:
+    if dict_PM["shaft"] != None:
+        if dict_PM["shaft"]["mat_type"]["is_isotropic"] == False:
             shaft_mat = {
-                "shaft_permeance": dict_SPMSM["shaft"]["mat_type"]["mag"]["BH_curve"][
+                "shaft_permeance": dict_PM["shaft"]["mat_type"]["mag"]["BH_curve"][
                     "value"
                 ]
             }  # non-linear case
         else:
             shaft_mat = {
-                "shaft_permeance": dict_SPMSM["shaft"]["mat_type"]["mag"]["mur_lin"]
+                "shaft_permeance": dict_PM["shaft"]["mat_type"]["mag"]["mur_lin"]
             }  # linear case
     else:
         shaft_mat = {"shaft_permeance": "Not defined"}
---
+++ pyleecan/Methods/Network/InputSpmsm/load_parameters.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+from pyleecan.Functions.load import load_json
+
+
+def load_parameters(file_path_json):
+    """Load the stator and rotor material characteristics of an PM machine
+
+    Parameters
+    ----------
+    file_path_json: str
+        path to the file of the machine to be loaded
+
+    Returns
+    -------
+    rotor_param: dict
+        Geometrical parameters of the rotor core and magnets
+    stator_param: dict
+        Geometrical parameters of the stator core and stator slots
+    """
+
+    # Load the SPMSM machine from the .json file
+    PM = load_json.load_json(file_path_json)
+
+    # Get the dictionary values with the different motor parameters
+    dict_PM = PM[1]
+
+    # Get Rotor parameters
+    rotor_param = {
+        "rotor_Rext": dict_PM["rotor"]["Rext"],
+        "rotor_Rint": dict_PM["rotor"]["Rint"],
+        "rotor_poles_Zs": dict_PM["rotor"]["slot"]["Zs"],
+        # Magnet
+        "magnet_height_Hmag": dict_PM["rotor"]["slot"]["Hmag"],
+        "magnet_width_Wmag": dict_PM["rotor"]["slot"]["Wmag"],
+    }
+
+    # Get Stator parameters
+    stator_param = {
+        "stator_Rext": dict_PM["stator"]["Rext"],
+        "stator_Rint": dict_PM["stator"]["Rint"],
+        # Stator slot
+        "stator_tooth_tip_height_H0": dict_PM["stator"]["slot"]["H0"],
+        "stator_tooth_tip_width_W0": dict_PM["stator"]["slot"]["W0"],
+        "stator_slot_length_H2": dict_PM["stator"]["slot"]["H2"],
+        "stator_slot_width_W2": dict_PM["stator"]["slot"]["W2"],
+        "stator_poles_Zs": dict_PM["stator"]["slot"]["Zs"],
+    }
+
+    return rotor_param, stator_param
