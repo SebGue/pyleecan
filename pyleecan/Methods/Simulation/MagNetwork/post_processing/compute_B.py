099dcf0167166223e3747a5c1302eb1c48fe9d78
Aicha Maaoui <aicha.maaoui@eomys.com>
2022-07-25 14:52:37 +0200

    [CR] Update comp_B

--- pyleecan/Methods/Simulation/MagNetwork/post_processing/compute_B.py
+++ pyleecan/Methods/Simulation/MagNetwork/post_processing/compute_B.py
@@ -26,7 +26,7 @@ def compute_B(self, Phi, list_elem, list_coord, la, type_coord_sys):
         y component of B
 
     """
-
+    if type_coord_sys == 1:
         h_x = np.linalg.norm(
             list_coord[list_elem[:, 0]] - list_coord[list_elem[:, 1]],
             axis=1,
@@ -51,12 +51,27 @@ def compute_B(self, Phi, list_elem, list_coord, la, type_coord_sys):
         return Bx, By
 
     if type_coord_sys == 2:
+
         h_theta = np.abs(
             list_coord[list_elem[:, 0], 0] - list_coord[list_elem[:, 1], 0]
         )
+
         r1 = list_coord[list_elem[:, 0], 1]
         r2 = list_coord[list_elem[:, 3], 1]
         h_r = np.abs(r2 - r1)
 
+        # Compute B
+        Btheta_down = (Phi[list_elem[:, 1]] - Phi[list_elem[:, 0]]) / (
+            h_theta * r1 * la
+        )
+        Btheta_upper = (Phi[list_elem[:, 2]] - Phi[list_elem[:, 3]]) / (
+            h_theta * r2 * la
+        )
 
+        Br_left = (Phi[list_elem[:, 0]] - Phi[list_elem[:, 3]]) / (h_r * la)
+        Br_right = (Phi[list_elem[:, 1]] - Phi[list_elem[:, 2]]) / (h_r * la)
 
+        Btheta = (Btheta_down + Btheta_upper) / 2
+        Br = (Br_left + Br_right) / 2
+
+        return Btheta, Br
