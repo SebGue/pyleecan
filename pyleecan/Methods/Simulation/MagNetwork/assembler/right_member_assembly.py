Add material_dict in geometry_motor method and do the corresponding changes in the other methods of MagNetwork. Correct the contour flux computation to take into account all the materials in the motor geometry

pyleecan/Methods/Simulation/MagNetwork/assembler/right_member_assembly.py
@@ -6,12 +6,11 @@ from scipy.sparse import coo_matrix
 
 def right_member_assembly(
     self,
-    list_elements_materials,
+    list_permeability_cell,  # cells materials
     Num_Unknowns,
     list_elem,
     list_coord,
     reluc_list,
-    list_permeability,
     Br,
     mu0,
     la,
@@ -20,37 +19,32 @@ def right_member_assembly(
     JB=None,
     JC=None,
 ):
-    """Vector assembler (RHS)
 
-    Parameters
-    ----------
-    list_elements_materials: nd-array (size: m of integers)
-        Material of each cell of the motor geometry
-    Num_Unknowns : nd-array (size: n of integers)
-        Unknowns in the linear system
-    list_elem : nd-array (size: m of integers)
-        Elements in the motor geometry
-    list_coord : nd-array (size: n * 2 (flat))
-        Coordinates of each point
-    Br : float
-        Remeanance flux density of the permanent magnet
-    mu0 : float
-        Permeability of the vaccum
+    #######################################################################################
+    # Number of PMs per period
+    #######################################################################################
+    # Get machine object
+    Machine = self.parent.machine
 
-    Returns
-    -------
-    RHS : nd-array, size: Num.unknowns.max()+1
-        Right member vector
+    if Machine.comp_periodicity_spatial()[1] == True:
+        periodicity = Machine.comp_periodicity_spatial()[0]
+    else:
+        periodicity = Machine.comp_periodicity_spatial()[0] / 2
 
-    """
+    # Number of PMs per period
+    nb_PM_per_period = round(Machine.rotor.get_pole_pair_number() / periodicity)
 
-    position_permeability_PM = len(list_permeability) - 3
-    mask_magnet = list_elements_materials == position_permeability_PM
-    mur_PM = list_permeability[position_permeability_PM]  # 4-1???????
+    # Relative permeabiltity of the PM
+    mur_PM = Machine.rotor.magnet.mat_type.mag.mur_lin
+    #######################################################################################
+
+    mask_magnet = list_permeability_cell == mur_PM
     N_unknowns = Num_Unknowns.max() + 1
 
-    # np.savetxt("cell_materials.csv", list_elements_materials)
-    # print("non-zero in mak magnet?", mask_magnet.sum())
+    #######################################################################################
+    # Modeling of the PM force according to the PM direction (y): Phi_PM = FMMPM
+
+    # Linear : Ref 1: https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=9473194
     if type_coord_sys == 1:
         h_x = np.linalg.norm(
             list_coord[list_elem[:, 0]] - list_coord[list_elem[:, 1]], axis=1, ord=2
@@ -58,8 +52,9 @@ def right_member_assembly(
         h_y = np.linalg.norm(
             list_coord[list_elem[:, 1]] - list_coord[list_elem[:, 2]], axis=1, ord=2
         )
-        FMMPM = 0.5 * Br * la * h_y[mask_magnet] / mur_PM
+        FMMPM = 2 * Br * h_y[mask_magnet] * la / mur_PM
 
+    # Radial: Ref 2: https://www.researchgate.net/publication/269405270_Modeling_of_a_Radial_Flux_PM_Rotating_Machine_using_a_New_Hybrid_Analytical_Model
     elif type_coord_sys == 2:
         R1 = list_coord[list_elem[:, 0], 1]
         R2 = list_coord[list_elem[:, -1], 1]
@@ -68,12 +63,12 @@ def right_member_assembly(
             list_coord[list_elem[:, 1], 0] - list_coord[list_elem[:, 0], 0]
         )
 
-        FMMPM = 1.05 * Br * la * sigma_R[mask_magnet] * 2
+        FMMPM = 2 * Br * sigma_R[mask_magnet] * la / mur_PM
 
-        ##Initailyze returned vector -> RHS
+        # Initialize returned vector -> RHS
         RHS = np.zeros(N_unknowns, dtype=np.float64)
 
-        ####Permanant Magnet assembling
+        # Permanant Magnet assembling
         index_unknowns_1 = Num_Unknowns[list_elem[mask_magnet, 0]]
         index_unknowns_2 = Num_Unknowns[list_elem[mask_magnet, 1]]
         index_unknowns_3 = Num_Unknowns[list_elem[mask_magnet, 2]]
@@ -85,9 +80,15 @@ def right_member_assembly(
         RHS[index_unknowns_4] += FMMPM * reluc_list[mask_magnet, 3]
 
     ####Winding assembling
+
+    if Machine.stator.winding.conductor.cond_mat.mag != None:
+        mu_winding = Machine.stator.winding.conductor.cond_mat.mag.mur_lin
+    else:
+        mu_winding = 1
+
     if JA is None and JB is None and JC is None:
         # Phase 1
-        mask_winding = list_elements_materials == 1
+        mask_winding = list_permeability_cell == mu_winding
 
         if type_coord_sys == 1:
             S = h_x[mask_winding] * h_y[mask_winding] / 4
@@ -110,7 +111,8 @@ def right_member_assembly(
         RHS[index_unknowns_4] += JA * S
 
         # Phase 2
-        mask_winding = list_elements_materials == 2
+        mask_winding = list_permeability_cell == mu_winding + 1
+        # here to be changed by the winding number bob1, bob2..
 
         if type_coord_sys == 1:
             S = h_x[mask_winding] * h_y[mask_winding] / 4
@@ -133,7 +135,7 @@ def right_member_assembly(
         RHS[index_unknowns_4] += JB * S
 
         ###Phase 3
-        mask_winding = list_elements_materials == 3
+        mask_winding = list_permeability_cell == mu_winding + 2
         if type_coord_sys == 1:
             S = h_x[mask_winding] * h_y[mask_winding] / 4
         elif type_coord_sys == 2:

pyleecan/Methods/Simulation/MagNetwork/geometry_motor.py
@@ -7,6 +7,7 @@ import numpy as np
 from pyleecan.Functions.load import load
 from scipy.fft import ifftn
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 
 def geometry_motor(self, N_point_theta, N_point_r, rotor_shift):
@@ -130,7 +131,7 @@ def geometry_motor(self, N_point_theta, N_point_r, rotor_shift):
     # Relative permeabiltity of the PM
     mur_PM = Machine.rotor.magnet.mat_type.mag.mur_lin
 
-    material_dict = {}
+    material_dict = defaultdict(float)
 
     material_dict["air"] = mu_air
     material_dict["stator"] = mu_stator
@@ -258,7 +259,11 @@ def geometry_motor(self, N_point_theta, N_point_r, rotor_shift):
     N_total_element = N_element_r * N_element_theta_kk
 
     # Attribute an array of N_total_element size of zeros to cells_materials
-    cells_materials = np.zeros(N_total_element, dtype=np.uint16)
+    cells_materials = np.zeros(N_total_element, dtype=np.float64)
+
+    """Add the geoemtry_disctint list to be able to plot the different motor elements
+     (even with them having the same permeabilities) in the view_contour_flux method"""
+    geometry_disctint = np.zeros(N_total_element, dtype=np.uint16)
 
     # Assignment of geometry elements
     for i in range(N_element_r):
@@ -268,6 +273,7 @@ def geometry_motor(self, N_point_theta, N_point_r, rotor_shift):
             for j in range(N_element_theta_kk):
                 num_element = N_element_theta_kk * i + j
                 cells_materials[num_element] = material_dict["rotor"]  # rotor material
+                geometry_disctint[num_element] = 3
 
         # Assignment of PM and the airgap between PMs elements
         elif i < (rotor_height + airgap_and_Pm_height - airgap_height):
@@ -277,6 +283,7 @@ def geometry_motor(self, N_point_theta, N_point_r, rotor_shift):
                 # Assignment of the first half of the airgap elements
                 if j < half_airgap_PM_width:
                     cells_materials[num_element] = material_dict["air"]  # air material
+                    geometry_disctint[num_element] = 2
 
                 elif j < (N_element_theta_kk - half_airgap_PM_width):
                     for PM_idx in range(nb_PM_per_period):
@@ -299,6 +306,7 @@ def geometry_motor(self, N_point_theta, N_point_r, rotor_shift):
                             cells_materials[num_element] = material_dict[
                                 "PM" + str(PM_idx + 1)
                             ]  # PM material
+                            geometry_disctint[num_element] = 4 + PM_idx
                         # Assignment of the airgaps between PMs elements
 
                         if (
@@ -318,16 +326,19 @@ def geometry_motor(self, N_point_theta, N_point_r, rotor_shift):
                             cells_materials[num_element] = material_dict[
                                 "air"
                             ]  # air material between the PMs
+                            geometry_disctint[num_element] = 2
 
                 # Assignment of the last half of the airgap
                 else:
                     cells_materials[num_element] = material_dict["air"]  # air material
+                    geometry_disctint[num_element] = 2
 
         # Assignment of the airgap between the PMs and the stator
         elif i < (rotor_height + airgap_and_Pm_height):
             for j in range(N_element_theta_kk):
                 num_element = N_element_theta_kk * i + j
                 cells_materials[num_element] = material_dict["air"]  # air
+                geometry_disctint[num_element] = 2
 
         elif i < N_element_r - stator_iron_height:
             for j in range(N_element_theta_kk):
@@ -335,6 +346,7 @@ def geometry_motor(self, N_point_theta, N_point_r, rotor_shift):
                 # Assignment of the elements in the first half tooth
                 if j < Half_tooth_width:
                     cells_materials[num_element] = material_dict["stator"]  # stator
+                    geometry_disctint[num_element] = 1
 
                 elif j <= (N_element_theta_kk - Half_tooth_width):
                     for slot_idx in range(nb_stator_teeth_per_period):
@@ -379,6 +391,11 @@ def geometry_motor(self, N_point_theta, N_point_r, rotor_shift):
                                         "winding"
                                         + str(slot_idx * nb_layers + layer + 1)
                                     ]
+                                    geometry_disctint[num_element] = (
+                                        (4 + nb_PM_per_period)
+                                        + layer
+                                        + slot_idx * nb_layers
+                                    )
                             break
 
                         # Assignment of tooth elements
@@ -386,16 +403,19 @@ def geometry_motor(self, N_point_theta, N_point_r, rotor_shift):
                             cells_materials[num_element] = material_dict[
                                 "stator"
                             ]  # stator
+                            geometry_disctint[num_element] = 1
 
                 # Assignment of the last half tooth element
                 else:
                     cells_materials[num_element] = material_dict["stator"]  # stator
+                    geometry_disctint[num_element] = 1
 
         # Assignment of the back-iron layer
         else:
             for j in range(N_element_theta_kk):
                 num_element = N_element_theta_kk * i + j
                 cells_materials[num_element] = material_dict["stator"]  # stator
+                geometry_disctint[num_element] = 1
 
     # Plotting of the geometry : Validation
     # ct = plt.pcolormesh(
@@ -406,4 +426,4 @@ def geometry_motor(self, N_point_theta, N_point_r, rotor_shift):
     # plt.colorbar()
     # plt.show()
 
-    return cells_materials, material_dict, N_point_theta
+    return cells_materials, material_dict, N_point_theta, geometry_disctint

pyleecan/Methods/Simulation/MagNetwork/run_radial.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jun  3 10:46:17 2022
-@author: LAP02
-"""
+
 from re import S
 import numpy as np
 import meshio
@@ -16,15 +13,7 @@ from pyleecan.Classes.MeshSolution import MeshSolution
 from pyleecan.Classes.SolutionMat import SolutionMat
 
 
-def run_radial(
-    self,
-    axes_dict,
-    Is_val=None,
-    type_coord_sys=2,
-    # N_point_r=37,
-    # Kmesh_fineness=1,
-    # rotor_shift=8,
-):
+def run_radial(self, axes_dict, Is_val=None, type_coord_sys=2):
     """
     Solve the MagNetwork in the case of radial coordinate system type
 
@@ -38,10 +27,6 @@ def run_radial(
         Stator current matrix accounting for magnetic periodicities
     type_coord_sys : integer (Default = 2)
         Type of the coordinate system : 1 for Cartesian and 2 for Radial
-    Kmesh_fineness : integer (Default = 2)
-        Density of mesh cells inside the geometry
-    rotor_shift : integer (Default = 8)
-        Number of rotor mesh cells to be shifted with respect to the stator
 
     Returns
     -------
@@ -54,11 +39,13 @@ def run_radial(
     By_airgap :
         Flux density of the airgap in the theta direction
     """
-    Machine = self.parent.machine
+    ###############################################################################
     # Geometry inputs
-    la = Machine.rotor.L1  # Active length (m)
-    Br = Machine.rotor.magnet.mat_type.mag.Brm20
-    mu0 = np.pi * 4e-7  # Permeability of vacuum (H/m)
+    ###############################################################################
+    Machine = self.parent.machine
+
+    # Active length (m)
+    la = Machine.rotor.L1
 
     # Definition of N_point_theta initial value
     if Machine.comp_periodicity_spatial()[1] == True:
@@ -80,18 +67,27 @@ def run_radial(
         N_point_theta, self.N_point_r, self.rotor_shift
     )[2]
 
+    # Material properties of PM and vaccum
+    Br = Machine.rotor.magnet.mat_type.mag.Brm20
+    mu0 = np.pi * 4e-7  # Permeability of vacuum (H/m)
+
+    ###############################################################################
+    # Definition of the r- and theta- axes
+    ###############################################################################
     # Definition of the r-axis
     r = np.linspace(Machine.rotor.Rint, Machine.stator.Rext, self.N_point_r)
 
-    # Definition of the theta axis
+    # Definition of the theta-axis
     theta = axes_dict["theta_primal"].get_values(is_smallestperiod=True)
 
     # Definition of the theta_dual and r_dual axes
     theta_dual = axes_dict["angle"].get_values(is_smallestperiod=True)
-    r_dual = (r[1:] + r[:-1]) / 2
     # theta_dual = (theta[1:] + theta[:-1]) / 2
+    r_dual = (r[1:] + r[:-1]) / 2
 
+    ###############################################################################
     # Definition of the boundary conditions
+    ###############################################################################
     BC = [
         "anti_periodic_condition",
         "homogeneous_Dirichlet_condition",
@@ -99,7 +95,9 @@ def run_radial(
         "homogeneous_Dirichlet_condition",
     ]
 
-    # Compute current densities
+    ###############################################################################
+    # Compute current densities: TODO
+    ###############################################################################
     if Is_val is not None:
         surface_active = self.parent.machine.stator.slot.comp_surface_active()
         JA = Is_val[0, :] / surface_active
@@ -110,10 +108,11 @@ def run_radial(
         JB = None
         JC = None
 
-    # solving the model using the solver_linear_model
+    ###############################################################################
+    # solving the MagNetwork simulation using the solver_linear_model method
+    ###############################################################################
     (
         Phi,
-        list_geometry,
         Num_unknowns,
         list_elem,
         permeability_cell,
@@ -125,7 +124,6 @@ def run_radial(
         r,
         theta_dual,
         r_dual,
-        self.rotor_shift,
         BC,
         self.geometry_motor,
         mu0,
@@ -136,6 +134,10 @@ def run_radial(
         JB=JB,
         JC=JC,
     )
+
+    ###############################################################################
+    #  Plotting the flux density contour of the electric motor
+    ###############################################################################
 
     # Transfomration of radial coordinates to cartesian to plot the flux density contour
     x = (list_coord[:, 1] * np.cos(list_coord[:, 0])).reshape(
@@ -149,10 +151,19 @@ def run_radial(
     list_cartesian_coord[:, 0] = x.flatten()
     list_cartesian_coord[:, 1] = y.flatten()
 
+    ###############################################################################
     # Plotting the flux density contour
+    ###############################################################################
+
+    # Getting the geometry elements from the geometry_motor method
+    list_geometry = self.geometry_motor(
+        N_point_theta, self.N_point_r, self.rotor_shift
+    )[3]
     self.view_contour_flux(Phi, x, y, N_point_theta, self.N_point_r, list_geometry)
 
-    # computing B radial
+    ###############################################################################
+    # computing the flux density B
+    ###############################################################################
     Bx, By = self.compute_B(Phi, list_elem, list_coord, la, type_coord_sys)
 
     B = np.stack((Bx, By), axis=-1)

pyleecan/Methods/Simulation/MagNetwork/solver_linear_model.py
@@ -36,7 +36,6 @@ def solver_linear_model(
     r,
     x_dual,
     y_dual,
-    rotor_shift,
     boundary_condition,
     geometry,
     mu0,
@@ -93,47 +92,50 @@ def solver_linear_model(
         Coordinates of the geometry
     """
 
+    ###############################################################################
+    # Initialization of the grid and the MagNetwork simulation parameters
+    ###############################################################################
+
+    # Starting the couting time
     t0 = time.perf_counter()
 
-    # initialize the cells of materials and their permeability
-    list_elem_materials = self.geometry_motor(N_point_theta, N_point_r, rotor_shift)[0]
-
-    permeability_materials = self.geometry_motor(N_point_theta, N_point_r, rotor_shift)[
-        1
-    ]
-
-    (list_coord, list_elem,) = self.init_mesh(
+    (
+        list_coord,
+        list_elem,
+        list_elem_permability,
+        list_boundary_condition,
+        Num_Unknowns,
+    ) = self.init_mesh(
         N_point_theta,
         N_point_r,
         theta,
         r,
+        boundary_condition,
     )
 
-    list_elem_permability = self.init_permeabilty_cell(
-        N_point_theta, N_point_r, permeability_materials, mu0, list_elem_materials
-    )
-
-    list_boundary_condition, Periodic_point = self.init_mesh_BC(
-        N_point_theta, N_point_r, boundary_condition
-    )
-
-    Num_Unknowns = self.numeroting_unknows(
-        list_elem, list_boundary_condition, Periodic_point
-    )
-
-    # Mesuring the performance time
+    # Ending the couting time
     t1 = time.perf_counter()
 
-    print("Assembly geometry:", np.round(t1 - t0, 5), "seconds")
+    # Measuring the performance time
+    print("Initialization of the problem:", np.round(t1 - t0, 5), "seconds")
+
+    ###############################################################################
+    # Saving the mesh
+    ###############################################################################
+
     self.save_mesh(
         list_elem_materials, Num_Unknowns, list_elem, theta, r, list_boundary_condition
     )
 
     t2 = time.perf_counter()
-    # Saving mesh time
+
+    # Saving the mesh time
     print("Save mesh:", np.round(t2 - t1, 5), "secondes")
 
-    # Assembly all matrice
+    ###############################################################################
+    # Assemblying all the matrices
+    ###############################################################################
+
     reluc_list = self.init_reluc(list_elem, list_coord, mu0, la, type_coord_sys)
     # print(reluc_list)
 
@@ -148,14 +150,16 @@ def solver_linear_model(
     t3 = time.perf_counter()
     print("Assembly matrix", np.round(t3 - t2, 5), "secondes")
 
-    # Assembly RHS containing the sources
+    ###############################################################################
+    # Assemblying the RHS containing the sources
+    ###############################################################################
+
     RHS = self.right_member_assembly(
-        list_elem_materials,
+        list_elem_permability,
         Num_Unknowns,
         list_elem,
         list_coord,
         reluc_list,
-        permeability_materials,
         Br,
         mu0,
         la,
@@ -170,7 +174,10 @@ def solver_linear_model(
     print("Assembly vector:", np.round(t4 - t3, 5), "secondes")
     print("Total :", np.round(t4 - t2, 5))
 
-    # Compute Solution
+    ###############################################################################
+    # Computing the solution Phi
+    ###############################################################################
+
     t3 = time.perf_counter()
     if Have_cholmod:
 
@@ -194,6 +201,7 @@ def solver_linear_model(
             np.linalg.norm(M_csr @ Phi - RHS, ord=2),
         )
 
+    # Adding the oundary conditions to the solution Phi
     Phi = self.add_BC_to_Phi(
         Phi,
         Num_Unknowns,
@@ -203,7 +211,6 @@ def solver_linear_model(
 
     return (
         Phi,
-        list_elem_materials,
         Num_Unknowns,
         list_elem,
         list_elem_permability,