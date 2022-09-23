e2625e49bd494a19d099ade1dcc8579c82fdcf83
Sebastian Günther <sebgue@gmx.net>
2022-09-23 16:18:03 +0200

    [CO] HoleM52R: add label + fix geo for gmsh

--- pyleecan/Methods/Slot/HoleM52R/build_geometry.py
+++ pyleecan/Methods/Slot/HoleM52R/build_geometry.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-
-from numpy import exp
-
 from ....Classes.Arc1 import Arc1
 from ....Classes.Segment import Segment
 from ....Classes.SurfLine import SurfLine
-from ....Functions.labels import HOLEV_LAB, HOLEM_LAB
+from ....Functions.labels import HOLEM_LAB, HOLE_LAB, MAG_LAB, ROTOR_LAB
+from ....Functions.labels import RIGHT_LAB, LEFT_LAB, TOP_LAB, BOT_LAB
+from ....Functions.labels import BOUNDARY_PROP_LAB as BND
 
 
 def build_geometry(self, alpha=0, delta=0, is_simplified=False):
@@ -36,6 +35,9 @@ def build_geometry(self, alpha=0, delta=0, is_simplified=False):
     vent_label = lam_label + "_" + surf_type + "_R" + str(R_id) + "-"
     mag_label = lam_label + "_" + HOLEM_LAB + "_R" + str(R_id) + "-"
 
+    MAG = ROTOR_LAB + "_" + MAG_LAB
+    HOLE = ROTOR_LAB + "_" + HOLE_LAB
+
     point_dict = self._comp_point_coordinate()
     Z1 = point_dict["Z1"]
     Z2 = point_dict["Z2"]
@@ -64,11 +66,19 @@ def build_geometry(self, alpha=0, delta=0, is_simplified=False):
     # Creation of the air curve
     curve_list = list()
     if is_radius:
-        curve_list.append(Arc1(Z1R, Z1L, self.R0))
+        curve_list.append(
+            Arc1(Z1R, Z1L, self.R0, prop_dict={BND: "_".join([HOLE, TOP_LAB, "0"])})
+        )
     curve_list.append(Segment(Z1L if is_radius else Z1, Z2))
     curve_list.append(Segment(Z2, Z3))
     curve_list.append(Segment(Z3, Z11))
-    curve_list.append(Segment(Z11, Z1R if is_radius else Z1))
+    curve_list.append(
+        Segment(
+            Z11,
+            Z1R if is_radius else Z1,
+            prop_dict={BND: "_".join([HOLE, TOP_LAB, "0"])},
+        )
+    )
     point_ref = (Z1 + Z2 + Z3 + Z11) / 4
     S1 = SurfLine(line_list=curve_list, point_ref=point_ref)
 
@@ -78,10 +88,24 @@ def build_geometry(self, alpha=0, delta=0, is_simplified=False):
         curve_list.append(Segment(Z3, Z11))
         curve_list.append(Segment(Z7, Z10))
     else:
-        curve_list.append(Segment(Z4, Z11))
-        curve_list.append(Segment(Z11, Z10))
-        curve_list.append(Segment(Z10, Z6))
-        curve_list.append(Segment(Z6, Z4))
+        curve_list.append(
+            Segment(Z4, Z3, prop_dict={BND: "_".join([MAG, LEFT_LAB, "0"])})
+        )
+        curve_list.append(
+            Segment(Z3, Z11, prop_dict={BND: "_".join([MAG, LEFT_LAB, "0"])})
+        )
+        curve_list.append(
+            Segment(Z11, Z10, prop_dict={BND: "_".join([MAG, TOP_LAB, "0"])})
+        )
+        curve_list.append(
+            Segment(Z10, Z7, prop_dict={BND: "_".join([MAG, RIGHT_LAB, "0"])})
+        )
+        curve_list.append(
+            Segment(Z7, Z6, prop_dict={BND: "_".join([MAG, RIGHT_LAB, "0"])})
+        )
+        curve_list.append(
+            Segment(Z6, Z4, prop_dict={BND: "_".join([MAG, BOT_LAB, "0"])})
+        )
     point_ref = (Z11 + Z4 + Z6 + Z10) / 4
     S2 = SurfLine(line_list=curve_list, label=mag_label + "T0-S0", point_ref=point_ref)
 
@@ -90,8 +114,16 @@ def build_geometry(self, alpha=0, delta=0, is_simplified=False):
     curve_list.append(Segment(Z7, Z8))
     curve_list.append(Segment(Z8, Z9R if is_radius else Z9))
     if is_radius:
-        curve_list.append(Arc1(Z9R, Z9L, self.R0))
-    curve_list.append(Segment(Z9L if is_radius else Z9, Z10))
+        curve_list.append(
+            Arc1(Z9R, Z9L, self.R0, prop_dict={BND: "_".join([HOLE, TOP_LAB, "0"])})
+        )
+    curve_list.append(
+        Segment(
+            Z9L if is_radius else Z9,
+            Z10,
+            prop_dict={BND: "_".join([HOLE, TOP_LAB, "0"])},
+        )
+    )
     curve_list.append(Segment(Z10, Z7))
     point_ref = (Z7 + Z8 + Z9 + Z10) / 4
     S3 = SurfLine(line_list=curve_list, point_ref=point_ref)
