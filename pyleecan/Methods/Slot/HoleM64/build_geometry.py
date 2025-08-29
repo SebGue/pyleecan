f1b4594149ec0f149343068dab340f2ba204a1ff
Sebastian Günther <sebgue@gmx.net>
2025-08-29 13:34:59 +0200

    [CC] allow for no magnet

--- pyleecan/Methods/Slot/HoleM64/build_geometry.py
+++ pyleecan/Methods/Slot/HoleM64/build_geometry.py
@@ -52,63 +52,70 @@ def build_geometry(self, alpha=0, delta=0, is_simplified=False):
     # Create all the surfaces for all the cases
 
     # upper surface
-    curve_list = list()
+    upper_lines = list()
     if self.W1 > self.R0:
-        curve_list.append(Segment(conjugate(ZM1), conjugate(Z12)))
+        upper_lines.append(Segment(conjugate(ZM1), conjugate(Z12)))
     if self.R0 != 0:
-        curve_list.append(
+        upper_lines.append(
             Arc2(begin=conjugate(Z12), center=conjugate(Z1c), angle=pi / 2)
         )
 
-    curve_list.append(Segment(conjugate(Z11), Z11))
+    upper_lines.append(Segment(conjugate(Z11), Z11))
     if self.R0 != 0:
-        curve_list.append(Arc2(begin=Z11, center=Z1c, angle=pi / 2))
+        upper_lines.append(Arc2(begin=Z11, center=Z1c, angle=pi / 2))
 
     if self.W1 > self.R0:
-        curve_list.append(Segment(Z12, ZM1))
-    curve_list.append(Segment(ZM1, conjugate(ZM1)))
+        upper_lines.append(Segment(Z12, ZM1))
+    upper_lines.append(Segment(ZM1, conjugate(ZM1)))
 
     point_ref = (Z11 + conjugate(ZM1)) / 2
-    S0 = SurfLine(line_list=curve_list, point_ref=point_ref)
+    S0 = SurfLine(line_list=upper_lines, point_ref=point_ref)
     S0.label = vent_label + "T0-S0"
 
     # lower surface
-    curve_list = list()
+    lower_lines = list()
     if self.W3 > self.R0:
-        curve_list.append(Segment(ZM2, Z21))
+        lower_lines.append(Segment(ZM2, Z21))
 
     if self.R0 != 0:
-        curve_list.append(Arc2(begin=Z21, center=Z2c, angle=pi / 2))
-    curve_list.append(Segment(Z22, conjugate(Z22)))
+        lower_lines.append(Arc2(begin=Z21, center=Z2c, angle=pi / 2))
+    lower_lines.append(Segment(Z22, conjugate(Z22)))
     if self.R0 != 0:
-        curve_list.append(
+        lower_lines.append(
             Arc2(begin=conjugate(Z22), center=conjugate(Z2c), angle=pi / 2)
         )
     if self.W3 > self.R0:
-        curve_list.append(Segment(conjugate(Z21), conjugate(ZM2)))
-    curve_list.append(Segment(conjugate(ZM2), ZM2))
+        lower_lines.append(Segment(conjugate(Z21), conjugate(ZM2)))
+    lower_lines.append(Segment(conjugate(ZM2), ZM2))
 
     point_ref = (Z21 + conjugate(ZM2)) / 2
-    S1 = SurfLine(line_list=curve_list, point_ref=point_ref)
+    S1 = SurfLine(line_list=lower_lines, point_ref=point_ref)
     S1.label = vent_label + "T1-S0"
 
     # magnet_0 surface
-    if self.magnet_0 and self.W2 != 0:
-        curve_list = list()
-        curve_list.append(Segment(conjugate(ZM1), ZM1))
-        curve_list.append(Segment(ZM1, ZM2))
-        curve_list.append(Segment(ZM2, conjugate(ZM2)))
-        curve_list.append(Segment(conjugate(ZM2), conjugate(ZM1)))
-        point_ref = (ZM1 + conjugate(ZM2)) / 2
-        SM = SurfLine(line_list=curve_list, point_ref=point_ref)
-        SM.label = mag_label + "T0-S0"
+    magnet_lines = list()
+    magnet_lines.append(Segment(conjugate(ZM1), ZM1))
+    magnet_lines.append(Segment(ZM1, ZM2))
+    magnet_lines.append(Segment(ZM2, conjugate(ZM2)))
+    magnet_lines.append(Segment(conjugate(ZM2), conjugate(ZM1)))
+    point_ref = (ZM1 + conjugate(ZM2)) / 2
+    SM = SurfLine(line_list=magnet_lines, point_ref=point_ref)
+    SM.label = mag_label + "T0-S0"
+
+    # hole without magnet_0
+    hole_lines = upper_lines[:-2]
+    hole_lines += [magnet_lines[1]]
+    hole_lines += lower_lines[:-2]
+    hole_lines += [magnet_lines[3]]
+    point_ref = (ZM1 + conjugate(ZM2)) / 2
+    S10 = SurfLine(line_list=hole_lines, point_ref=point_ref)
+    S10.label = vent_label + "T0-S0"
 
     # Create the surface list by selecting the correct ones
-    surf_list = list()
-    surf_list += [S0]
-    if self.magnet_0 and self.W2 != 0:
-        surf_list += [SM]
-    surf_list += [S1]
+    if self.magnet_0:
+        surf_list = [S0, SM, S1]
+    else:
+        surf_list = [S10]
 
     # Apply the transformations
     for surf in surf_list:
