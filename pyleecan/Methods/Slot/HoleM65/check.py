102d60082a4d6b56e596249d12a718db12a77311
Sebastian Günther <sebgue@gmx.net>
2025-09-29 17:24:58 +0200

    [CC] fix HoleM65.check

--- pyleecan/Methods/Slot/HoleM65/check.py
+++ pyleecan/Methods/Slot/HoleM65/check.py
@@ -17,21 +17,26 @@ def check(self):
     Raises
     _______
     S65_WCheckError
-        You must have W1+H0 <= W2
+        You must have W4 > R
     """
     # Check that everything is set
-    if self.W0 is None:
-        raise S65_NoneError("You must set W0 !")
-    elif self.W1 is None:
+    if self.W1 is None:
         raise S65_NoneError("You must set W1 !")
     elif self.W2 is None:
         raise S65_NoneError("You must set W2 !")
     elif self.W3 is None:
         raise S65_NoneError("You must set W3 !")
+    elif self.W4 is None:
+        raise S65_NoneError("You must set W4 !")
     elif self.H0 is None:
         raise S65_NoneError("You must set H0 !")
     elif self.H1 is None:
         raise S65_NoneError("You must set H1 !")
+    elif self.R is None:
+        raise S65_NoneError("You must set R !")
 
-    if (self.W1 + self.H0) > self.W2:
-        raise S65_WCheckError("You must have (W1 + H0) <= W2")
+    if self.W4 <= self.R:
+        raise S65_WCheckError("You must have W4 > R")
+
+    if self.H1 <= (2 * self.R):
+        raise S65_WCheckError("You must have H1 > 2 * R")
